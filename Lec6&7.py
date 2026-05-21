7eae58b4b01e9caffae45cf7e0e1d3ac3e9891a6
Sumaira Ramzan <sumaira.ramzan244@gmail.com>
2026-05-21 17:34:34 +0500

    Add files via upload

---
+++ Lec6&7.py
@@ -0,0 +1,50 @@
+name="Sumaira "
+age=24
+height=5.4
+is_Trainer=True
+
+print("My name is ",name + "and I am ",age," years old. My height is ",height," and it is ",is_Trainer," that I am a trainer.")
+
+
+
+
+# data types
+
+# int = integer or whole number   (e.g. 1, 2, 3, -4, 0)
+# float = decimal number (e.g. 3.14, -0.5, 2.0)
+# str = string or text used for write anything in single or double qoutes (e.g. "Hello", 'World', "123")
+# bool = boolean or true/false (e.g. True, False)
+# Tuple = (1, 2, 3) # ordered and unchangeable collection of items
+# List = [1, 2, 3] # ordered and changeable collection of items
+# Dict = {"name": "Sumaira", "age": 23} # collection of key-value pairs
+
+
+# operators
+
+num1=11
+num2=3
+
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+# print date of birth of sumaira task 
+
+current_year=2026
+date_of_birth=current_year - age
+print("Sumaira's date of birth is ",date_of_birth)
+
+
+print("--------------")
+num3=34.3
+num4=45.3
+
+print(num3 // num4)
+print(num3 % num4)
+print(num3 ** num4)
+
+print("------- Age -------")
+print("Sumaira's DOB is ",num1, "/" ,num2, "/" ,date_of_birth)
+
